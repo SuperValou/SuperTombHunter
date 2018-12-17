dbbeb0803ed3b67baf2f6c092cd8e7b1bd8cc930
vesmieu <valentin.esmieu@ubisoft.com>
2018-12-17 16:10:18 +0100

    Add Grid prefab

---
+++ PartyGame/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	public GameObject prefab;
+	
+	public int size = 4;
+		
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				var clone = Instantiate(prefab, new Vector3(i, j, 0), Quaternion.identity);
+				clone.name = "tile x" + i + " y" + j;
+			}				
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
