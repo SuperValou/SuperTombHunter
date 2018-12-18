0feae2ea3dedca77f9578415f06e53ca0d2900bc
vesmieu <valentin.esmieu@ubisoft.com>
2018-12-18 10:47:01 +0100

    Create teams gameobjects

--- PartyGame/Assets/Scripts/Teams/Team.cs
+++ PartyGame/Assets/Scripts/Teams/Team.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts.Teams
 {
     public class Team : MonoBehaviour
     {
         public TeamSide TeamSide;
+
+        public Text ScoreLabel;
+
+        public int Score { get; set; }
     }
 }
