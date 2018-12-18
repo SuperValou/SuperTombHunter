4592e232b14cecab28592a89b5e1f5d1e8572f8e
vesmieu <valentin.esmieu@ubisoft.com>
2018-12-18 17:48:12 +0100

    Add Timer

---
+++ PartyGame/Assets/Scripts/UI/GlobalTimer.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    public class GlobalTimer : MonoBehaviour
+    {
+        public int maxTime = 90;
+
+        public UiBar uiBar;
+
+        private Stopwatch _stopwatch = new Stopwatch();
+        
+        void Start()
+        {
+            _stopwatch.Start();
+        }
+        
+        void Update()
+        {
+            int timeToDisplay = maxTime - (int) _stopwatch.Elapsed.TotalSeconds;
+
+            if (timeToDisplay <= 0)
+            {
+                timeToDisplay = 0;
+            }
+
+            uiBar.DisplaySeconds(timeToDisplay);
+        }
+
+        public void Restart()
+        {
+            _stopwatch.Restart();
+        }
+    }
+}
---
+++ PartyGame/Assets/Scripts/UI/UiBar.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    public class UiBar : MonoBehaviour
+    {
+        public SpriteRenderer[] numbers;
+
+        private Transform _tensLocation;
+        private Transform _unitsLocation;
+
+        private readonly Dictionary<int, SpriteRenderer> _tensSprites = new Dictionary<int, SpriteRenderer>();
+        private readonly Dictionary<int, SpriteRenderer> _unitsSprites = new Dictionary<int, SpriteRenderer>();
+
+        void Start()
+        {
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                var numberSprite = numbers[i];
+
+                var tensClone = Instantiate(numberSprite, _tensLocation.position, Quaternion.identity);
+                _tensSprites.Add(i, tensClone);
+
+                var unitsClone = Instantiate(numberSprite, _tensLocation.position, Quaternion.identity);
+                _unitsSprites.Add(i, unitsClone);
+            }
+        }
+
+        public void DisplaySeconds(int secondsToDisplay)
+        {
+            var ten = secondsToDisplay / 10;
+            var unit = secondsToDisplay % 10;
+
+            var tenToDisable = ten + 9 % 10;
+            var unitToDisable = unit + 9 % 10;
+
+            _tensSprites[tenToDisable].gameObject.SetActive(false);
+            _tensSprites[ten].gameObject.SetActive(true);
+
+            _unitsSprites[unitToDisable].gameObject.SetActive(false);
+            _unitsSprites[unit].gameObject.SetActive(true);
+        }
+    }
+}
