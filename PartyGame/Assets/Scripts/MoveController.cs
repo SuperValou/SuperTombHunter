1f790838d6e106aa88e2323d511e081666e47ed7
Johanny <johanny.clerc-renaud@ubisoft.com>
2018-12-18 19:15:41 +0100

    Animating player 1

--- PartyGame/Assets/Scripts/MoveController.cs
+++ PartyGame/Assets/Scripts/MoveController.cs
@@ -12,7 +12,16 @@ public class MoveController : MonoBehaviour
     private string VerticalAxis;
     private string GrabAxis;
 
-    void Start()
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
     {
         if (player == null)
         {
@@ -44,5 +53,44 @@ public class MoveController : MonoBehaviour
         Vector3 movement = new Vector3(horizontal, vertical, 0f).normalized;
         Vector3 acceleration = movement * speed * Time.deltaTime;
         gameObject.transform.Translate(acceleration);
+
+        FlipSprite(horizontal);
+
+        if (movement.y > 0.01f)
+        {
+            animator.SetBool("moveUp", true);
+            animator.SetBool("moveDown", false);
+            animator.SetBool("moveHorizontaly", false);
+        }
+
+        if (movement.y < -0.01f)
+        {
+            animator.SetBool("moveUp", false);
+            animator.SetBool("moveDown", true);
+            animator.SetBool("moveHorizontaly", false);
+        }
+        
+        if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y))
+        {
+            animator.SetBool("moveUp", false);
+            animator.SetBool("moveDown", false);
+            animator.SetBool("moveHorizontaly", true);
+        }
+
+        if (Mathf.Abs(movement.x) < 0.01f && Mathf.Abs(movement.y) < 0.01f)
+        {
+            animator.SetBool("moveUp", false);
+            animator.SetBool("moveDown", false);
+            animator.SetBool("moveHorizontaly", false);
+        }
+    }
+
+    void FlipSprite(float horizontal)
+    {
+        bool flipSprite = spriteRenderer.flipX ? horizontal < 0.01f : horizontal > 0.01f;
+        if (flipSprite)
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
     }
 }
