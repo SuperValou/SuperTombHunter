765a299fb007eee5e6c98e9aaf043b4965b35240
vesmieu <valentin.esmieu@ubisoft.com>
2018-12-19 10:47:58 +0100

    Plug ScoreRecap

--- PartyGame/Assets/Scripts/UI/ScoreRecap.cs
+++ PartyGame/Assets/Scripts/UI/ScoreRecap.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Assets.Scripts.Teams;
 using UnityEngine;
 
 public class ScoreRecap : MonoBehaviour
 {
+    public SpriteRenderer hotWinnerSprite;
+    public SpriteRenderer coldWinnerSprite;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        var winner = PlayerPrefs.GetString("Winner");
+        if (winner == TeamSide.Hot.ToString())
+        {
+            coldWinnerSprite.gameObject.SetActive(false);
+        }
+        else
+        {
+            hotWinnerSprite.gameObject.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
