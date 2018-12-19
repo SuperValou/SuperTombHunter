5616ed69f931a0d9d0f2a03b1a8b262228fc62b8
Johanny <johanny.clerc-renaud@ubisoft.com>
2018-12-19 12:29:18 +0100

    Better inputs

--- PartyGame/Assets/Scripts/UI/TitleScreen.cs
+++ PartyGame/Assets/Scripts/UI/TitleScreen.cs
@@ -5,17 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class TitleScreen : MonoBehaviour
 {
-    private readonly string[] _buttons = new []{ "P0_Grab", "P1_Grab", "P2_Grab", "P3_Grab"} ;
-    
     void Update()
     {
-        foreach (var button in _buttons)
+        if (Input.GetButtonDown("MenuValid"))
         {
-            if (Input.GetButtonDown(button))
-            {
-                Debug.Log(button);
-                SceneManager.LoadScene(1);
-            }
+            SceneManager.LoadScene(1);
         }
     }
 }
