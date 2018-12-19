23875702a85012360c262d3eabb404d30318e528
Johanny <johanny.clerc-renaud@ubisoft.com>
2018-12-19 13:22:43 +0100

    Fix Input bugs in build !!!

--- PartyGame/Assets/Scripts/MoveController.cs
+++ PartyGame/Assets/Scripts/MoveController.cs
@@ -8,10 +8,6 @@ public class MoveController : MonoBehaviour
     public int JoystickNumber;
     public float speed = 5f;
 
-    private string HorizontalAxis;
-    private string VerticalAxis;
-    private string GrabAxis;
-
     private SpriteRenderer spriteRenderer;
     private Animator animator;
 
@@ -29,23 +25,17 @@ public class MoveController : MonoBehaviour
         }
     }
 
-    public void InitAxis()
-    {
-        HorizontalAxis = "P" + JoystickNumber + "_Horizontal";
-        VerticalAxis = "P" + JoystickNumber + "_Vertical";
-        GrabAxis = "P" + JoystickNumber + "_Grab";
-    }
+    public void InitAxis() {}
 
     void Update()
     {
-        if (Input.GetButtonDown(GrabAxis))
+        if (Input.GetButtonDown("P" + JoystickNumber + "_Grab"))
         {
-            Debug.Log("P" + JoystickNumber + " is performing an action");
             player.GrabDropAction();
         }
 
-        float horizontal = Input.GetAxis(HorizontalAxis);
-        float vertical = Input.GetAxis(VerticalAxis);
+        float horizontal = Input.GetAxis("P" + JoystickNumber + "_Horizontal");
+        float vertical = Input.GetAxis("P" + JoystickNumber + "_Vertical");
 
         Vector3 movement = new Vector3(horizontal, vertical, 0f).normalized;
         Vector3 acceleration = movement * speed * Time.deltaTime;
